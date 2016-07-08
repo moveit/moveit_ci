c747c72fa520ce2a38e4c1053e23efa85e284ca8
Dave Coleman <davetcoleman@gmail.com>
2016-07-08 12:27:51 -0600

    Intregrated current MoveIt! CI script

--- travis.sh
+++ travis.sh
@@ -2,42 +2,16 @@
 
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2016, Isaac I. Y. Saito, Dave Coleman
-# All rights reserved.
+# Inspired by MoveIt! travis https://github.com/ros-planning/moveit_core/blob/09bbc196dd4388ac8d81171620c239673b624cc4/.travis.yml
+# Inspired by JSK travis https://github.com/jsk-ros-pkg/jsk_travis
+# Inspired by ROS Industrial https://github.com/ros-industrial/industrial_ci
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#       * Redistributions of source code must retain the above copyright
-#       notice, this list of conditions and the following disclaimer.
-#       * Redistributions in binary form must reproduce the above copyright
-#       notice, this list of conditions and the following disclaimer in the
-#       documentation and/or other materials provided with the distribution.
-#       * Neither the name of the Isaac I. Y. Saito, nor the names
-#       of its contributors may be used to endorse or promote products derived
-#       from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Greatly inspired by JSK travis https://github.com/jsk-ros-pkg/jsk_travis
-# Greatly inspired by ROS Industrial: https://github.com/ros-industrial/industrial_ci
-#
-# Author: Dave Coleman, Isaac I. Y. Saito
+# Author:  Dave Coleman, Jonathan Bohren, Robert Haschke, Isaac I. Y. Saito
 
 export CI_SOURCE_PATH=$(pwd)
 export CI_PARENT_DIR=.ci_config  # This is the folder name that is used in downstream repositories in order to point to this repo.
 export HIT_ENDOFSCRIPT=false
-export DOWNSTREAM_REPO_NAME=${PWD##*/}
+export REPOSITORY_NAME=${PWD##*/}
 
 # Helper functions
 source ${CI_SOURCE_PATH}/$CI_PARENT_DIR/util.sh
@@ -45,17 +19,18 @@ source ${CI_SOURCE_PATH}/$CI_PARENT_DIR/util.sh
 # Run all CI in a Docker container
 if ! [ "$IN_DOCKER" ]; then
     # Pull first to allow us to hide console output
-    #docker pull davetcoleman/moveit_ci:moveit-$ROS_DISTRO-ci > /dev/null
-    docker pull davetcoleman/moveit_ci:moveit-$ROS_DISTRO-ci
+    #docker pull moveit/moveit_docker:moveit-$ROS_DISTRO-ci > /dev/null
+    docker pull moveit/moveit_docker:moveit-$ROS_DISTRO-ci
 
     # Start Docker container
-    docker run -v $(pwd):/root/$DOWNSTREAM_REPO_NAME davetcoleman/moveit_ci:moveit-$ROS_DISTRO-ci \
+    docker run -v $(pwd):/root/$REPOSITORY_NAME moveit/moveit_docker:moveit-$ROS_DISTRO-ci \
         -e ROS_REPOSITORY_PATH \
         -e ROS_DISTRO \
         -e BEFORE_SCRIPT \
         -e CI_PARENT_DIR \
         -e UPSTREAM_WORKSPACE \
-        /bin/bash -c "cd /root/$DOWNSTREAM_REPO_NAME; source .ci_config/travis.sh;"
+        -e TRAVIS_BRANCH \
+        /bin/bash -c "cd /root/$REPOSITORY_NAME; source .ci_config/travis.sh;"
     return_value=$?
 
     if [ $return_value -eq 0 ]; then
@@ -66,7 +41,9 @@ if ! [ "$IN_DOCKER" ]; then
     echo "ROS $ROS_DISTRO Docker container finished with errors"
     exit -1 # error
 fi
+
 # If we are here, we can assume we are inside a Docker container
+echo "Testing branch $TRAVIS_BRANCH of $REPOSITORY_NAME on $ROS_DISTRO"
 
 # Set apt repo - this was already defined in OSRF image but we probably want shadow-fixed
 if [ ! "$ROS_REPOSITORY_PATH" ]; then # If not specified, use ROS Shadow repository http://wiki.ros.org/ShadowRepository
@@ -82,10 +59,10 @@ travis_run sudo apt-get -qq update
 travis_run rosdep update
 
 # Create workspace
-travis_run mkdir -p ~/ros/ws_$DOWNSTREAM_REPO_NAME/src
-travis_run cd ~/ros/ws_$DOWNSTREAM_REPO_NAME/src
+travis_run mkdir -p ~/ros/ws_$REPOSITORY_NAME/src
+travis_run cd ~/ros/ws_$REPOSITORY_NAME/src
 
-# Install any prerequisites or dependencies necessary to run build
+# Install dependencies necessary to run build using .rosinstall files
 if [ ! "$UPSTREAM_WORKSPACE" ]; then
     export UPSTREAM_WORKSPACE="debian";
 fi
@@ -109,16 +86,16 @@ esac
 # download upstream packages into workspace
 if [ -e .rosinstall ]; then
     # ensure that the downstream is not in .rosinstall
-    travis_run wstool rm $DOWNSTREAM_REPO_NAME || true
+    travis_run wstool rm $REPOSITORY_NAME || true
     travis_run cat .rosinstall
     travis_run wstool update
 fi
 
-# CI_SOURCE_PATH is the path of the downstream repository that we are testing. Link it to the catkin workspace
+# link in the repo we are testing
 travis_run ln -s $CI_SOURCE_PATH .
 
 # source setup.bash
-travis_run source /opt/ros/$ROS_DISTRO/setup.bash
+#travis_run source /opt/ros/$ROS_DISTRO/setup.bash
 
 # Run before script
 if [ "${BEFORE_SCRIPT// }" != "" ]; then sh -c "${BEFORE_SCRIPT}"; fi
@@ -127,13 +104,14 @@ if [ "${BEFORE_SCRIPT// }" != "" ]; then sh -c "${BEFORE_SCRIPT}"; fi
 travis_run sudo rosdep install -r -y -q -n --from-paths . --ignore-src --rosdistro $ROS_DISTRO
 
 # Change to base of workspace
-travis_run cd ~/ros/ws_$DOWNSTREAM_REPO_NAME/
+travis_run cd ~/ros/ws_$REPOSITORY_NAME/
 
 # re-source setup.bash for setting environmet vairable for package installed via rosdep
 #travis_run source /opt/ros/$ROS_DISTRO/setup.bash
 
 # Configure catkin to use install configuration
-travis_run catkin config --install
+#travis_run catkin config --install
+travis_run catkin config --extend /opt/ros/$ROS_DISTRO --install --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 # Console output fix for: "WARNING: Could not encode unicode characters"
 PYTHONIOENCODING=UTF-8
@@ -156,7 +134,7 @@ travis_run catkin build --no-status --summarize --make-args tests -- $TEST_PKGS
 
 # Run tests
 travis_run catkin run_tests --no-status --summarize $TEST_PKGS
-travis_run catkin_test_results
+catkin_test_results || $ALLOW_TEST_FAILURE
 
 echo "Travis script has finished successfully"
 HIT_ENDOFSCRIPT=true
