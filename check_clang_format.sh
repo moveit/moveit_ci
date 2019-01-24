44c2ad6082e93b3f4f7a741d319cc21765951018
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2019-01-24 14:40:11 +0100

    improve display of clang-format results

--- check_clang_format.sh
+++ check_clang_format.sh
@@ -2,8 +2,7 @@
 travis_run apt-get -qq install -y clang-format-3.9
 
 # Change to source directory.
-travis_run cd $CI_SOURCE_PATH
-travis_run ls -la
+cd $CI_SOURCE_PATH
 
 # This directory can have its own .clang-format config file but if not, MoveIt's will be provided
 if [ ! -f .clang-format ]; then
@@ -11,17 +10,16 @@ if [ ! -f .clang-format ]; then
 fi
 
 # Run clang-format
-echo "Running clang-format"
+travis_time_start moveit_ci.clang-format "Running clang-format"  # start fold
 find . -name '*.h' -or -name '*.hpp' -or -name '*.cpp' | xargs clang-format-3.9 -i -style=file
-
-echo "Showing changes in code style:"
-git --no-pager diff
+travis_time_end  # end fold
 
 # Make sure no changes have occured in repo
 if ! git diff-index --quiet HEAD --; then
     # changes
-    echo "clang-format test failed: changes required to comply to formatting rules. See diff above.";
+    echo -e "\033[31;1mclang-format test failed: The following changes are required to comply to rules:\033[0m"
+    git --no-pager diff
     exit 1 # error
 fi
 
-echo "Passed clang-format test"
+echo -e "\033[32;1mPassed clang-format check\033[0m"
