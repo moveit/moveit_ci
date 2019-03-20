b2d6a98c697064399062f23c3cde52e3b00f2df9
Mike Lautman <mikeblautman@github.com>
2019-03-19 18:55:00 -0600

    porting check warnings

--- check_warnings.sh
+++ check_warnings.sh
@@ -6,9 +6,9 @@
 # Desc: Check for warnings during build process of repo $CI_SOURCE_PATH
 
 packages_with_warnings() {
-   for pkg in $(catkin_topological_order $CI_SOURCE_PATH --only-names 2> /dev/null) ; do
+   for pkg in $(colcon info | grep 'name: ' | sed -e "s/.*name: //g" 2> /dev/null) ; do
       # Warnings manifest themselves log files in catkin tools' logs folder
-      files=$(find $ROS_WS/logs/$pkg -name "*build.cmake.000.log.stderr" -o -name "*build.make.00[01].log.stderr" 2> /dev/null)
+      files=$(find $ROS_WS/log/latest_build/$pkg -name "stderr.log" 2> /dev/null)
       # Extract types of failures from file names
       issues=""
       issues="${issues}$(echo $files | sed -ne 's:.*/build\.cmake\.000.*:cmake :p')"
